f2ba0421f3e574bf8adb4453c9279deeb789a566
Derek Brown <derek@derek.am>
2014-12-24 03:11:22 -0800

    Minor Changes

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,5 @@
-// export default DS.RESTAdapter.extend({
-//   host: "http://flatwhite.dev:9000"
-// });
+export default DS.RESTAdapter.extend({
+  host: "http://flatwhite.dev:9000"
+});
 
-export default DS.FixtureAdapter.extend({});
+// export default DS.FixtureAdapter.extend({});
