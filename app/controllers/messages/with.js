4efe39db3af7b31427f0ce01c3c05ea54753340b
Derek Brown <derek@derek.am>
2015-01-07 15:30:11 -0800

    Fix Linting Issue (Semi-Colon)

--- app/controllers/messages/with.js
+++ app/controllers/messages/with.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   url: 'flatwhite.dev:9000/messages/with/',
   model: function(params) {
-    return Ember.$.getJSON( this.url + params.participant)
+    return Ember.$.getJSON( this.url + params.participant);
   }
 });
